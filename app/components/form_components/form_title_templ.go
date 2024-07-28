76c68c8f31e5bd15737b5a90b83c2f2e95965e2f
BaguetteGod <mikavm@outlook.com>
2024-07-28 22:17:08 +0200

    Styling changes

--- app/components/form_components/form_title_templ.go
+++ app/components/form_components/form_title_templ.go
@@ -26,14 +26,14 @@ func FormTitle(title string, description string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div><h2 class=\"text-gray-700 leading-7 font-medium\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div><h2 class=\"text-gray-700 leading-7 font-medium text-base\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(title)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/components/form_components/form_title.templ`, Line: 5, Col: 57}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/components/form_components/form_title.templ`, Line: 5, Col: 67}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
